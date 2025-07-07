3df474ffc34c64075904099e338cbd52da4dcde6
Aleksandra <a.petkova1617@gmail.com>
2025-07-07 18:07:35 +0100

    Adding the door meshes on the door BP

--- Source/LevelUpJam/Obstacles/Obstacle.cpp
+++ Source/LevelUpJam/Obstacles/Obstacle.cpp
@@ -9,16 +9,16 @@ AObstacle::AObstacle()
  	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-	RootComponent = Mesh;
-
 	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
-	Collider->SetupAttachment(Mesh);
+	RootComponent = Collider;
 	Collider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	Collider->SetCollisionResponseToAllChannels(ECR_Ignore);
 	Collider->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 
 	Collider->OnComponentBeginOverlap.AddDynamic(this, &AObstacle::HandleBeginOverlap);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Collider);
 }
 
 void AObstacle::Activate()
